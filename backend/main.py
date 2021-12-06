6c590e37a1f6ae7a112cd5619f6f3062129feb4f
mayukh551 <may14ukh@gmail.com>
2021-12-06 19:29:37 +0530

    Beta-Test_#1

--- backend/main.py
+++ backend/main.py
@@ -1,107 +1,165 @@
+"""
+    This is the main python file...
+
+    ->  It gets the historical data of actors and directors
+        of the movie searched by the user
+
+    ->  And then based on their average score and best performances,
+        It is checked how famous, good, or flop these actors or directors are...
+
+    ->  Based on these factors, a movie is rated as hit/good/average/flop
+
+"""
+
 import requests as r
 import os
-from imdb_Package import *
 from Get_Actor_List import *
-# from GetActorsList import *
+from Director_Movies_Score import *
+from Get_Cast_Score import *
+
+
 # c4779b30
 
-# Fetching api keys from different websites
 
-omdb_api_key = os.environ.get('OMDB_API')
-tmdb_api_key = os.environ.get('MOVIE_API')
-movie_name = input("Enter the movie name : ")
-print('file-1')
-# From OMDB website to fetch release year,
-# all kinds of ratings,
-# awards,
-# cast and director and type
+def extract_best_movies(ratings):
+    s = 0
+    average = 0.0
+    no_of_scores = len(ratings)
+    # s is score out of 10
+    # So is average
+    if no_of_scores >= 4:
+        s = ratings[-1] + ratings[-2] + ratings[-3]
+        average = s / 3
+    elif no_of_scores == 3:
+        s = sum(ratings[1:])
+        average = s / 2
+    elif no_of_scores == 2:
+        average = sum(ratings) / 2
+    elif no_of_scores == 1:
+        average = sum(ratings)
+    return average
 
-url = "http://www.omdbapi.com/?apikey=" + omdb_api_key + "&t=" + movie_name + "&plot=full"
 
-response = r.get(url)
-movie_data = response.json()
+def main_file_run(movie_name, y):
+    # Fetching api keys from different websites
 
-release_year = movie_data['Year']
-print("Release Of Year", release_year)
-director = movie_data['Director']
-content_type = movie_data['Type']
+    omdb_api_key = os.environ.get('OMDB_API')
+    tmdb_api_key = os.environ.get('MOVIE_API')
 
-#  Fetch List of all actors
-cast_list = findCast(movie_name, release_year)
-if cast_list == 0:
-    print("Program Stopped!")
-else:
-    # extracting only top five actors
-    cast_list = cast_list[:5]
-    cast_list.append(director)
-    print(*cast_list)
-    # printing list of actors
-    for person in cast_list:
-        findMovies(person)
-    print('\nfile-1\n')
-    # printing list of actors along with list of movies
-    # of each actor
-    print(actor_hist)
-    actor_avg_score = {}
-    director_avg_score = {}
-    for i, j in actor_hist.items():
+    '''
+    From OMDB website to fetch release year,
+    all kinds of ratings,
+    awards,
+    cast and director and type'''
+
+    # + omdb_api_key +
+
+    # movie_name = input("Enter the movie name : ")
+    # y = input("Enter the release year: ")
+    print('file-1')
+
+    url = "http://www.omdbapi.com/?apikey=c4779b30&t=" + movie_name + "&y=" + y + "&plot=short"
+    response = r.get(url)
+    movie_data = response.json()
+
+    release_year = ''
+
+    if y == '':
+        release_year = movie_data['Year']
+        print("Release Of Year", release_year)
+    else:
+        release_year = y
+
+    director = movie_data['Director']
+
+    #  Fetch List of all actors
+    cast_list = findCast(movie_name, release_year)
+    if len(cast_list) == 0:
+        print("Program Stopped!")
+        cast_list = movie_data['Actors']
+    else:
         score = []
-        #  printing actor name
-        print(i)
-        # printing actor's movies list (p) along with release year (q)
-        for p, q in j.items():
-            print(p, ':', q)
+        actor_score = {}
+        # if cast List not empty
+        score_pref = {}
+        actor_performance = {}
+        print('Cast : ', cast_list)
+        for actor in cast_list:
+            # returns score list of past movies
+            score = main_code(actor)
+            actor_avg = extract_best_movies(score)
+            # storing actor's average score along with no. of movies (MAX : 5)
+            actor_performance.setdefault(actor_avg, len(score))
 
-            # Calling OMDB API to fetch imdb ratings
-            # of last 5 movies of each actor
-            url = "http://www.omdbapi.com/?apikey=" + omdb_api_key + "&t=" + str(p) + "&y=" + str(q) + "&plot=full"
-            response = r.get(url)
-            movie_data = response.json()
-            if 'N' not in movie_data['imdbRating']:
-                #  storing imdb score in a list
-                score.append(float(movie_data['imdbRating']))
-        print()
-        # Find the average of best 3 movies
-        score.sort()
-        # Last 3 elements will be highest of all
-        print(score)
-        score_sum = sum(score[2:])
-        avg = score_sum / 3
-        print(f'Average rating of {i} : {avg}')
-        actor_avg_score.setdefault(i, avg)
-        print()
-    print(director_hist)
-    for i, j in director_hist.items():
-        score = []
-        #  printing actor name
-        print(i)
-        # printing actor's movies list (p) along with release year (q)
-        for p, q in j.items():
-            print(p, ':', q)
+        # Score preference based on no. of movies and score
+        count_newbies = 0
+        best_actor_scores = []
 
-            # Calling OMDB API to fetch imdb ratings
-            # of last 5 movies of each actor
-            url = "http://www.omdbapi.com/?apikey=" + omdb_api_key + "&t=" + str(p) + "&y=" + str(q) + "&plot=full"
-            response = r.get(url)
-            movie_data = response.json()
-            if 'N' not in movie_data['imdbRating']:
-                #  storing imdb score in a list
-                score.append(float(movie_data['imdbRating']))
-        print()
-        # Find the average of best 3 movies
-        score.sort()
-        # Last 3 elements will be highest of all
-        print(score)
-        score_sum = score[-1] + score[-2] + score[-3]
-        avg = score_sum / 3
-        print(f'Average rating of {i} : {avg}')
-        director_avg_score.setdefault(i, avg)
-        print()
+        # For loop to accept experienced actors first
+        for i, j in actor_performance.items():
+            # i -> avg score of an actor
+            # j -> no. of movies (MAX 5)
+            if j > 4:
+                best_actor_scores.append(i / 2)
 
-    for i, j in actor_avg_score.items():
-        print(f'{i} : %.2f' % j)
+        # Checking if cast list have experienced actors
+        if len(best_actor_scores) > 1:
+            # Scores arranged in descending order
+            best_actor_scores.sort(reverse=True)
 
-    for i, j in director_avg_score.items():
-        print(f'{i} : %.2f' % j)
+            if len(best_actor_scores) >= 3:
+                best_actor_scores = best_actor_scores[:3]
 
+        # if there is lack of experienced actors
+        # newbies are considered
+        if len(best_actor_scores) < 3:
+            for i, j in actor_performance.items():
+                # actors with less experience
+                if j < 4:
+                    best_actor_scores.append(i / 2)
+                if len(best_actor_scores) == 3:
+                    break
 
+        # A function to extract a list of scores
+        # of last 5 movies of a director by passing director name
+        scoreOfMovies = director_main_code(director)
+
+        # scoreOfMovies is score of movies of a director
+        # stores the avg of best movies out of 5 or less
+        director_avg = extract_best_movies(scoreOfMovies)
+        director_avg = director_avg / 2
+
+        # best 3 actor chosen out of 5
+        p = ok = flop = 0
+        for i in best_actor_scores:
+            if 3.5 <= i <= 5:
+                # p counts no. of good actors
+                p = p + 1
+            elif 3 <= i <= 3.5:
+                # ok counts no. of average actors
+                ok = ok + 1
+            else:
+                # flop counts no. of flop actors
+                flop = flop + 1
+
+        dp = dok = dflop = 0
+        i = director_avg
+        if 3.5 <= i <= 5:
+            dp = dp + 1
+        elif 3 <= i <= 3.5:
+            dok = dok + 1
+        else:
+            dflop = dflop + 1
+
+        print('Based on cast popularity and past movies success')
+        print('This movie will be ', end=" ")
+        if p >= 2 and dp == 1:
+            print("Hit")
+        elif p >= 1 and dp == 1 and ok >= 1:
+            print('Above Average')
+        elif p >= 1 or dp == 1:
+            print('Average')
+        elif p >= 1 and dok == 1:
+            print('Below Average')
+        else:
+            print('Flop')
